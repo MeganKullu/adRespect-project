fa7a1463835d60e8c49a825b725c667d78444b0b
MeganKullu <kullumegan@gmail.com>
2023-08-15 16:41:25 +0300

    homepage complete

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        "beige" : "#DCC1AB",
+        "green" : "#1B5B31",
+        "grey": "F5F0EC",
+        "black": "111111",
+      },
+      fontFamily : {
+        sans : ['Montserrat', 'sans-serif'],
+        serif: ['Inter', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
