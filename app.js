c494109e3f89418160533d333a9e837ff4e281a6
MeganKullu <kullumegan@gmail.com>
2023-08-21 09:38:45 +0300

    added anime script

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-const myElement = document.getElementById("myElement");
 
-// Define the animation
-anime({
-  targets: myElement,
-  opacity: 0.5,
-  scale: 1.2,
-  duration: 1000, // Animation duration in milliseconds
-  easing: "easeInOutQuad" // Animation easing function
-});
 
