a5ec895dd454f9a492d21cebd45c18be0d84f6c9
MeganKullu <kullumegan@gmail.com>
2023-08-21 16:28:08 +0300

    added mobile navigation

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+const previousButton = document.getElementById("data-carousel-previous");
+    const nextButton = document.getElementById("data-carousel-next");
+    const carouselItems = document.querySelectorAll("[data-carousel-item]");
+    let currentItemIndex = 0;
 
+    previousButton.addEventListener("click", () => {
+        carouselItems[currentItemIndex].classList.add("hidden");
+        currentItemIndex = (currentItemIndex - 1 + carouselItems.length) % carouselItems.length;
+        carouselItems[currentItemIndex].classList.remove("hidden");
+    });
+
+    nextButton.addEventListener("click", () => {
+        carouselItems[currentItemIndex].classList.add("hidden");
+        currentItemIndex = (currentItemIndex + 1) % carouselItems.length;
+        carouselItems[currentItemIndex].classList.remove("hidden");
+    });
 
